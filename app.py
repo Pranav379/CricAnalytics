d47bdcedd8680c9134c81124cb831c6d169c1bd5
Karan Bhalla <karanbhalla204@gmail.com>
2024-10-25 14:03:51 -0500

    Add app.py for main application setup

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "IPL Hawkeye Dashboard",
+    layout = "centered"
+)
+
+
+# Title on the page
+# st.markdown("# IPL Hawkeye Dashboard")        # Same thing but with markdown
+st.title("IPL Hawkeye Dashboard")
+
