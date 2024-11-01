d94aecc4f943cccdac0dab138473d8c42dc30ec9
PaninGitHub <diniduk@outlook.com>
2024-11-01 14:13:25 -0500

    Added some featues and designed function

--- batting/matchups.py
+++ batting/matchups.py
@@ -3,22 +3,33 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 df = pd.read_csv('data/ipl-hawkeye-data.csv')
-# def get_matchup(player):
-#    player_data = df[df['bat'] == player] #Filters for specific player
-#    avg_scores = player_data.groupby('bowl_type')['score'].mean().reset_index()
-#    avg_scores = avg_scores.rename(columns={'score': 'average_runs'})
-#    return avg_scores
-
 
 # Calculates the strike rate 
-def get_matchup_stats(player_name, bowling_type, pace_min= None, pace_max= None):
-    player_data = df[df['bat'] == player_name]
+def get_matchup_stats(player_name, bowling_type, pace_min= None, pace_max= None, deviation_min=None, deviation_max=None):
+    #fetches specific player
+    player_data = df[(df['bat'] == player_name) & (df['bowl_style'] == bowling_type)]
+    #accounts for pace calculations
     if pace_max != None:
         player_data = df.loc[(df['release_speed'] < pace_max)]
     if pace_min != None:
         player_data = df.loc[(df['release_speed'] > pace_min)]
-    strike_r_by_player = player_data.groupby('bowl_style')['batruns'].mean().mul(100).reset_index()        
-    return strike_r_by_player
+    #accounts for deviation
+    if deviation_max != None:
+        player_data = df.loc[(df['deviation'] < deviation_max)]
+    if pace_min != None:
+        player_data = df.loc[(df['deviation'] > pace_min)]
+    #calculates strike rate by getting bowl_style
+    player_vs_bowl = player_data.groupby('bowl_style')['batruns']
+    total_balls = player_vs_bowl.shape[0]
+    strike_r_by_player = player_vs_bowl.mean().mul(100)    
+    #formats stats and return
+    stats = {
+        "Total Balls": total_balls,
+        "Strike Rate": strike_r_by_player
+    }
+    return stats
 
 
-
+#Create a bar chart
+s = get_matchup_stats('Virat Kohli', 'RM')
+print(s)
