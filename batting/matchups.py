1a7d8f3aba89de2b25888d15456c8fee60012173
PaninGitHub <diniduk@outlook.com>
2024-11-01 14:28:40 -0500

    Added matchup function for player against bowling type

--- batting/matchups.py
+++ batting/matchups.py
@@ -2,34 +2,31 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv('data/ipl-hawkeye-data.csv')
-
 # Calculates the strike rate 
-def get_matchup_stats(player_name, bowling_type, pace_min= None, pace_max= None, deviation_min=None, deviation_max=None):
+def get_matchup_stats(df, player_name, bowling_type, pace_min= None, pace_max= None, deviation_min=None, deviation_max=None):
     #fetches specific player
     player_data = df[(df['bat'] == player_name) & (df['bowl_style'] == bowling_type)]
     #accounts for pace calculations
-    if pace_max != None:
+    if pace_max is not None:
         player_data = df.loc[(df['release_speed'] < pace_max)]
-    if pace_min != None:
+    if pace_min is not None:
         player_data = df.loc[(df['release_speed'] > pace_min)]
     #accounts for deviation
-    if deviation_max != None:
+    if deviation_max is not None:
         player_data = df.loc[(df['deviation'] < deviation_max)]
-    if pace_min != None:
+    if pace_min is not None:
         player_data = df.loc[(df['deviation'] > pace_min)]
     #calculates strike rate by getting bowl_style
-    player_vs_bowl = player_data.groupby('bowl_style')['batruns']
-    total_balls = player_vs_bowl.shape[0]
-    strike_r_by_player = player_vs_bowl.mean().mul(100)    
+    total_balls = player_data.shape[0]
+    total_runs = player_data['batruns'].sum()
+    #Avoids divison by zero
+    if total_balls == 0:
+        strike_rate = None
+    else:
+        strike_rate = round((total_runs / total_balls) * 100)
     #formats stats and return
     stats = {
         "Total Balls": total_balls,
-        "Strike Rate": strike_r_by_player
+        "Strike Rate": strike_rate
     }
     return stats
-
-
-#Create a bar chart
-s = get_matchup_stats('Virat Kohli', 'RM')
-print(s)
