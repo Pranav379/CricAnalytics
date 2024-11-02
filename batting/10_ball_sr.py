58462f01e9378b80b9741676b3725fda51f3ce6b
AnanthP44 <140008477+AnanthP44@users.noreply.github.com>
2024-11-01 19:28:13 -0500

    Removed unnessecary constants and added comments

--- batting/10_ball_sr.py
+++ batting/10_ball_sr.py
@@ -2,25 +2,26 @@
 import pandas as pd
 
 
-df = pd.read_csv("C:/Users/ananth/PycharmProjects/pythonProject/ipl-hawkeye-data.csv")
-
-
 def calculate_10_ball_sr(df, batter_name):
     batter_data = df[df['bat'] == batter_name]
     if 'batruns' not in batter_data.columns:
         return "Column 'batruns' not found in data."
-    
+        
+    #filters for only the first 10 balls
     first_10 = batter_data.head(10)
-
+    
+    #sums up score for those first 10 balls
     total_runs = first_10['batruns'].sum()
 
     if len(first_10) < 10:
         return None
     else:
         strike = (total_runs/10)*100
-        return strike
+        
+        #gets the strikerate
+        SR = {
+        "10 Ball Sr": strike
+    }
+        
+        return SR
     
-
-batter_name = input("Enter a batter's name (must be in the format: Firstname Lastname): ")
-strike = calculate_10_ball_sr(df, batter_name)
-print(f'10-ball SR for {batter_name}: {strike}')
