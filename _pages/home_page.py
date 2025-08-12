0f0e6861de124627f2e108c17e6953862c68e7d1
Pranav <pranavb07@gmail.com>
2025-08-12 14:58:23 -0500

    Changed dimensions

--- _pages/home_page.py
+++ _pages/home_page.py
@@ -35,7 +35,7 @@ st.markdown(
     }}
     .left-image {{
         position: absolute;
-        left: -250px;
+        left: -200px;
         top: 50px;
         width: 600px;   /* increased width and matching right image */
         height: 750px;  /* container height */
