cea63c8cceb66233da26cc3d287744f083da9381
Pranav <pranavb07@gmail.com>
2025-08-12 12:59:04 -0500

    Ball next to text

--- _pages/home_page.py
+++ _pages/home_page.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import base64
 
 st.set_page_config(
     page_title="IPL Hawkeye Dashboard",
@@ -9,12 +10,17 @@ st.title("IPL Hawkeye Dashboard")
 st.markdown("## Welcome to this interactive dashboard analyzing cricket players in the 2023 and 2024 Indian Premier League (IPL) Seasons!")
 st.markdown('## Click on "Batter Hub" and "Bowler Hub" on the sidebar to explore further')
 
-# Inline heading with cricket ball image that scales with text
+# Convert image to Base64
+with open("images/cricketball.png", "rb") as f:
+    img_bytes = f.read()
+encoded_img = base64.b64encode(img_bytes).decode()
+
+# Inline heading with cricket ball that scales with text
 st.markdown(
-    """
+    f"""
     <h1 style='display: flex; align-items: center; gap: 10px;'>
         Get ready for a chase!
-        <img src='https://github.com/Pranav379/CricAnalytics/blob/main/images/cricketball.png' style='height: 1em;'>
+        <img src='data:image/png;base64,{encoded_img}' style='height: 1em;'>
     </h1>
     """,
     unsafe_allow_html=True
