cacc286c54a848b0cf30a1b9e517d3fde007251c
Pranav <pranavb07@gmail.com>
2025-08-12 12:51:16 -0500

    Ball next to text

--- _pages/home_page.py
+++ _pages/home_page.py
@@ -1,14 +1,19 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title = "IPL Hawkeye Dashboard",
-    layout = "centered"
+    page_title="IPL Hawkeye Dashboard",
+    layout="centered"
 )
 
 st.title("IPL Hawkeye Dashboard")
 st.markdown("## Welcome to this interactive dashboard analyzing cricket players in the 2023 and 2024 Indian Premier League (IPL) Seasons!")
 st.markdown('## Click on "Batter Hub" and "Bowler Hub" on the sidebar to explore further')
-st.markdown("# Get ready for a chase!")
 
-image_path = "images/cricketball.png"
-st.image(image_path)
+# Create two columns for text and image
+col1, col2 = st.columns([4, 1])  # Adjust ratio for spacing
+
+with col1:
+    st.markdown("# Get ready for a chase!")
+
+with col2:
+    st.image("images/cricketball.png", use_column_width=True)
