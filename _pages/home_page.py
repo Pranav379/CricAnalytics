2cf269a6cfc337bafe2c29287cfe3db5a378a00a
Pranav <pranavb07@gmail.com>
2025-08-12 12:56:15 -0500

    Ball next to text

--- _pages/home_page.py
+++ _pages/home_page.py
@@ -9,11 +9,13 @@ st.title("IPL Hawkeye Dashboard")
 st.markdown("## Welcome to this interactive dashboard analyzing cricket players in the 2023 and 2024 Indian Premier League (IPL) Seasons!")
 st.markdown('## Click on "Batter Hub" and "Bowler Hub" on the sidebar to explore further')
 
-# Create two columns for text and image
-col1, col2 = st.columns([4, 1])  # Adjust ratio for spacing
-
-with col1:
-    st.markdown("# Get ready for a chase!")
-
-with col2:
-    st.image("images/cricketball.png")
+# Inline heading with cricket ball image that scales with text
+st.markdown(
+    """
+    <h1 style='display: flex; align-items: center; gap: 10px;'>
+        Get ready for a chase!
+        <img src='images/cricketball.png' style='height: 1em;'>
+    </h1>
+    """,
+    unsafe_allow_html=True
+)
