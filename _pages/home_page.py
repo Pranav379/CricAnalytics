fd96208164dce8e4068952732fe75518a793f07e
Pranav379 <pranavb07@gmail.com>
2025-08-11 21:14:35 -0500

    Update home_page.py

--- _pages/home_page.py
+++ _pages/home_page.py
@@ -7,5 +7,5 @@ st.set_page_config(
 
 st.title("IPL Hawkeye Dashboard")
 st.markdown("## Welcome to this interactive dashboard analyzing cricket players in the 2023 and 2024 Indian Premier League (IPL) Seasons!")
-st.markdown('Click on "Batter Hub" and "Bowler Hub" on the sidebar to explore further')
+st.markdown('## Click on "Batter Hub" and "Bowler Hub" on the sidebar to explore further')
 st.markdown("## Get ready for a chase!")
