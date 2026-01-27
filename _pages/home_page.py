3f97ffaccbeef8c74783223ec2499e435f36a179
Pranav <pranavb07@gmail.com>
2026-01-27 10:05:38 -0600

    Fixed text error

--- _pages/home_page.py
+++ _pages/home_page.py
@@ -21,21 +21,54 @@ st.markdown(
         to {{ transform: rotate(360deg); }}
     }}
 
-    .ball-container {{
+    .container {{
         display: flex;
         justify-content: center;
+        align-items: flex-start;
+        max-width: 1200px;
+        margin: auto;
+        padding: 50px 20px;
+        min-height: 80vh;
+    }}
+
+    .middle-content {{
+        max-width: 800px;
+        text-align: left;
+    }}
+
+    .heading {{
+        display: flex;
         align-items: center;
-        height: 80vh;
+        gap: 12px;
+        margin-top: 20px;
+        font-size: 2.5rem;
+        font-weight: 600;
     }}
 
     .spin {{
-        height: 120px;
+        height: 1.5em;
         animation: spin 0.75s linear infinite;
     }}
     </style>
 
-    <div class="ball-container">
-        <img src="data:image/png;base64,{encoded_cricketball}" class="spin" />
+    <div class="container">
+        <div class="middle-content">
+            <h1>IPL Hawkeye Dashboard</h1>
+
+            <h2>
+                Welcome to this interactive dashboard analyzing cricket players
+                in the 2023 and 2024 Indian Premier League (IPL) Seasons!
+            </h2>
+
+            <h2>
+                Click on "Batter Hub" and "Bowler Hub" on the sidebar to explore further
+            </h2>
+
+            <div class="heading">
+                <span>Get ready for a chase!</span>
+                <img src="data:image/png;base64,{encoded_cricketball}" class="spin" />
+            </div>
+        </div>
     </div>
     """,
     unsafe_allow_html=True,
