e131a0c3b164a126c0f2db0d544f6a1172fcb167
Pranav <pranavb07@gmail.com>
2026-01-27 10:16:27 -0600

    Resolved alignment

--- _pages/home_page.py
+++ _pages/home_page.py
@@ -23,11 +23,20 @@ components.html(
 
     body {{
         font-family: sans-serif;
+        margin: 0;
+    }}
+
+    .wrapper {{
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
     }}
 
     .content {{
         max-width: 900px;
-        margin: 60px auto;
+        text-align: center;
+        padding: 20px;
     }}
 
     .title {{
@@ -44,6 +53,7 @@ components.html(
 
     .heading {{
         display: flex;
+        justify-content: center;
         align-items: center;
         gap: 12px;
         font-size: 2rem;
@@ -57,24 +67,26 @@ components.html(
     }}
     </style>
 
-    <div class="content">
-        <div class="title">IPL Hawkeye Dashboard</div>
+    <div class="wrapper">
+        <div class="content">
+            <div class="title">IPL Hawkeye Dashboard</div>
 
-        <div class="text">
-            Welcome to this interactive dashboard analyzing cricket players
-            in the 2023 and 2024 Indian Premier League (IPL) Seasons!
-        </div>
+            <div class="text">
+                Welcome to this interactive dashboard analyzing cricket players
+                in the 2023 and 2024 Indian Premier League (IPL) Seasons!
+            </div>
 
-        <div class="text">
-            Click on <strong>"Batter Hub"</strong> and <strong>"Bowler Hub"</strong>
-            on the sidebar to explore further
-        </div>
+            <div class="text">
+                Click on <strong>Batter Hub</strong> and <strong>Bowler Hub</strong>
+                on the sidebar to explore further
+            </div>
 
-        <div class="heading">
-            <span>Get ready for a chase!</span>
-            <img src="data:image/png;base64,{encoded_cricketball}" class="spin">
+            <div class="heading">
+                <span>Get ready for a chase!</span>
+                <img src="data:image/png;base64,{encoded_cricketball}" class="spin">
+            </div>
         </div>
     </div>
     """,
-    height=600,
+    height=700,
 )
